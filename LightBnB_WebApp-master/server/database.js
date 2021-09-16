bdacf0ab774a1269c18fb730230840a4ab175614
MavFeuille <mavis.feuille@gmail.com>
2021-09-16 14:24:59 -0400

    Update 2 functions: getUserWithID and addUser in database.js to use parameterized query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -54,7 +54,23 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  
+  const queryString = `
+  SELECT * 
+  FROM users
+  WHERE id = $1;
+  `
+  return pool
+    .query(queryString, [id])
+    .then((result) => {
+      console.log("Result.rows: ", result.rows)
+      return result.rows[0];
+    })
+    .catch((err) => null);
+
+
+  
+  // return Promise.resolve(users[id]);
 }
 exports.getUserWithId = getUserWithId;
 
@@ -65,10 +81,26 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  const queryString =  `
+  INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *;
+  `;
+
+  const values = [user.name, user.email, user.password];
+
+  return pool
+    .query(queryString, values)
+    .then((result) => {
+      console.log("Result.rows: ", result.rows)
+      return result.rows[0];
+    })
+    .catch((err) => console.log(err.message))
+
+  // const userId = Object.keys(users).length + 1;
+  // user.id = userId;
+  // users[userId] = user;
+  // return Promise.resolve(user);
 }
 exports.addUser = addUser;
 
